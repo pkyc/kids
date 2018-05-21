c00f409f4eea4f09a8266debbedd4ab6ff98ab79
pkyc <pkyc@users.noreply.github.com>
2018-05-21 10:26:03 +0800

    Update dbconnect.php

--- v3/dbconnect.php
+++ v3/dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "password", "db") or die("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "root", "password", "db2") or die("Error " . mysqli_error($con));
 ?>
